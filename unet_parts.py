17aaf704a7cb105c4efee03af2cd60fddac3ed28
Atharv Bhat <bhat.atharv@gmail.com>
2021-12-07 13:32:31 -0500

    Actually changed files kekl

--- unet_parts.py
+++ unet_parts.py
@@ -74,4 +74,4 @@ class OutConv(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        return self.conv(x)
+         return self.conv(x)
